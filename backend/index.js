8bb24a6289c0d1dddee54a3ed7920dcf6aa16490
Vipul Kumar <vipulkumar3885@gmail.com>
2025-11-19 13:47:03 +0530

    Update CORS origin to include new domain

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://vibefit-one.onrender.com" , "http://localhost:5174"],
+ origin:["https://vibefit-one.onrender.com" , "https://vibefit-adminonee.onrender.com"],
  credentials:true
 }))
 
