d3a02de74f63e7acb44104ff1b4c7151fef64d5f
Vipul Kumar <vipulkumar3885@gmail.com>
2026-02-14 13:25:44 +0530

    Update CORS origin to include new domain

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://vibefit-froontend.onrender.com" , "http://localhost:5174"],
+ origin:["https://vibefit-froontend.onrender.com" , "https://vibefit-1aadmin.onrender.com"],
  credentials:true
 }))
 
